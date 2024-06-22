6e9d77beb83cf742f5014a4f4a5cfb0ec1cb73b3
Premium Partners <145288968+redaqsd@users.noreply.github.com>
2024-06-22 17:17:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,49 +14,6 @@ app.get("/" , async(req,res) => {
     res.status(200).json({dikr})
 })
 
-app.post("/" , async(req,res) => {
-    const dikr = await Dikr.create(req.body)
-    res.status(201).json({dikr})
-})
-
-app.get("/:id" , async(req,res) => {
-    const {id : dikrID} = req.params
-    const dikr = await Dikr.findOne({_id : dikrID})
-
-    if(!dikr){
-        res.status(400).send(`No Dikr Found With The Following ID : ${dikrID}`)
-    }
-
-    res.status(201).json({dikr})
-})
-
-app.delete("/:id" , async(req,res) => {
-    const {id : dikrID} = req.params
-    const dikr = await Dikr.findByIdAndDelete({_id : dikrID})
-
-    if(!dikr){
-        res.status(400).send(`No Dikr Found With The Following ID : ${dikrID}`)
-    }
-
-    res.status(201).json({dikr})
-})
-
-app.patch("/:id" , async(req,res) => {
-    const {id : dikrID} = req.params
-    const options = {
-        new : true,
-        runValidators : true
-    }
-    const dikr = await Dikr.findOneAndUpdate({_id : dikrID} , req.body , options)
-
-    if(!dikr){
-        res.status(400).send(`No Dikr Found With The Following ID : ${dikrID}`)
-    }
-
-    res.status(200).json({dikr})
-})
-
-
 const connectDB = (url) => {
     mongoose.connect(url , console.log("Connected to DB!"))
 }
